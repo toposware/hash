0df1d55b6a351c7ca2e54b66c53df2e8c3f90be4
Robin Salen <salenrobin@gmail.com>
2021-12-10 17:37:59 +0900

    Benchmark hashing of 10KB of random data

--- benches/rescue-63-14-7.rs
+++ benches/rescue-63-14-7.rs
@@ -3,11 +3,20 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 extern crate hash;
 use hash::rescue_63_14_7::{digest::RescueDigest, hasher::RescueHash};
 use hash::traits::Hasher;
+use rand::thread_rng;
+use rand::RngCore;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let v: [RescueDigest; 2] = [RescueHash::hash(&[1u8]), RescueHash::hash(&[2u8])];
     c.bench_function("rescue-63-14-7 - merge", |bench| {
         bench.iter(|| RescueHash::merge(black_box(&v)))
+    });
+
+    c.bench_function("rescue-63-14-7 - hash 10KB", |bench| {
+        let mut data = vec![0u8; 10 * 1024];
+        let mut rng = thread_rng();
+        rng.fill_bytes(&mut data);
+        bench.iter(|| RescueHash::hash(black_box(&data)))
     });
 }
 
