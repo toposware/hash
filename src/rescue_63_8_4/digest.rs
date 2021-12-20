615847ace7a3391624291ce897de5b7b8e171d2e
Robin Salen <salenrobin@gmail.com>
2021-12-20 16:44:45 +0900

    Swap name and date in copyright mentions

--- src/rescue_63_8_4/digest.rs
+++ src/rescue_63_8_4/digest.rs
@@ -1,4 +1,4 @@
-// Copyright (c) Toposware, Inc. 2021
+// Copyright (c) 2021 Toposware, Inc.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
