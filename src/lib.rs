f0f13cbfddd643efd1d8208e741ad7f186c7b976
Robin Salen <salenrobin@gmail.com>
2022-08-18 11:21:28 -0400

    Update crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,26 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! An implementation of different cryptographic hash functions.
+//! This crate provides an implementation of different cryptographic
+//! algebraic hash functions.
+//!
+//! This library is intended to serve as a mid-level API for cryptographic
+//! protocols requiring algebraic hash functions, for instance in zk-SNARK
+//! or zk-STARK proving systems.
+//!
+//! All hash instantiations are defined using a `Hasher` trait and can both
+//! process sequences of bytes or native field elements.
+//!
+//! # Features
+//!
+//! The `f64` feature, not activated by default, allows to compile hash
+//! instantiations defined over the Cheetah primefield (also known as
+//! Goldilocks field) Fp with p = 2**64 - 2**32 + 1.
+//!
+//! The `hash` library by default relies on the Rust standard library.
+//! To make it suitable for use in embedded systems or WASM environments,
+//! one should disable the feature by using `--no-default-features`. This
+//! will make the library rely on the `alloc` crate instead for `Vec` support.
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![deny(rustdoc::broken_intra_doc_links)]
